bab1f5c5ae26c713c6a46bb4e572b6cdca202108
Joel <139776060+jjmayorga12345@users.noreply.github.com>
2024-02-04 23:44:25 -0500

    update
    update

---
+++ justincase/justincase.js
@@ -0,0 +1,22 @@
+<script>
+var highScoreList = [
+    {name: 'Joel', score: 1000},
+    {name: 'Leynadth', score: 980}
+];
+
+var menus = document.querySelector('#menus');
+
+var message = '';
+
+for (var i = 0; i < highScoreList.length; i++) {
+    var highscorePlayer = highScoreList[i];
+
+
+    message += `
+        <div id="high-scores">
+            Player: ${highscorePlayer.name}, Score: ${highscorePlayer.score}
+        </div>`;
+}
+
+menus.innerHTML = message;
+</script>
