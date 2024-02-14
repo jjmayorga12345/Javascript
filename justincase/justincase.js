1d3e07a44fda1c6aced5087dc6ff20e2a471acf3
Joel <139776060+jjmayorga12345@users.noreply.github.com>
2024-02-13 22:38:43 -0500

    up
    up

--- justincase/justincase.js
+++ justincase/justincase.js
@@ -20,3 +20,27 @@ for (var i = 0; i < highScoreList.length; i++) {
 
 menus.innerHTML = message;
 </script>
+
+<script>
+var highScoreList = [
+    {name: 'Joel', score: 1000},
+    {name: 'Leynadth', score: 980}
+];
+
+var menus = document.querySelector('#menus');
+
+
+var message = '';
+
+
+for (var i = 0; i < highScoreList.length; i++) {
+    var highscorePlayer = highScoreList[i];
+
+    var playerHs = document.createElement('div');
+    playerHs.setAttribute('id', 'high-scores');
+    playerHs.textContent = `
+        Player: ${highscorePlayer.name}, Score: ${highscorePlayer.score}`;
+
+    menus.appendChild(playerHs);
+}
+</script>
